bd8a44c56da3eedb9253f67b7862f2b6faf6de57
NoivernAlchemist <hallale21@yahoo.com>
2018-10-28 19:17:09 -0500

    Add character bullet "collision detection" with boss and a scuffed timer to remove health

--- Adept Fantasia/PlayScene.swift
+++ Adept Fantasia/PlayScene.swift
@@ -9,8 +9,11 @@
 import SpriteKit
 import GameplayKit
 import CoreMotion
+import UIKit
 
 class PlayScene: SKScene {
+    
+    var crescendoCategory:UInt32 = 0x1 << 0 //1
     
     var characterTexture = SKTexture(imageNamed: "CharacterImage.png")
     var bossTexture = SKTexture(imageNamed: "BossImage.png")
@@ -27,13 +30,22 @@ class PlayScene: SKScene {
     var charBulletTexture = SKTexture(imageNamed: "Bullet1.png")
     
     var bossHealthPercentage: Float = 0.0
-    var bossHealth = 258
+    var bossHealth = 50000
     var unfilledBossHealthBarTexture = SKTexture(imageNamed: "UnfilledBossHealthBar.png")
     var filledBossHealthBarTexture = SKTexture(imageNamed: "FilledBossHealthBar.png")
+    let bossHealthLabel = SKLabelNode()
     
     let motionManager: CMMotionManager = CMMotionManager()
     
     override func didMove(to view: SKView) {
+        
+        bossHealthLabel.text = "Boss Health:  \(bossHealth)"
+        bossHealthLabel.fontName = "Baskerville"
+        bossHealthLabel.fontSize = 60
+        bossHealthLabel.fontColor = .green
+        bossHealthLabel.position = CGPoint(x: -107, y: self.size.height/4 + 270)
+        bossHealthLabel.zPosition = 1
+        addChild(bossHealthLabel)
         
         bossHealthBar = SKSpriteNode(texture: filledBossHealthBarTexture)
         bossHealthBar.position = CGPoint(x: 0, y: self.size.height/4 + 240)
@@ -60,6 +72,9 @@ class PlayScene: SKScene {
         let timer = Timer.scheduledTimer(timeInterval: 0.007, target: self, selector: #selector(moveBossInALine), userInfo: nil, repeats: true)
         timer.fire()
         
+        let charBulletTimer = Timer.scheduledTimer(timeInterval: 0.9, target: self, selector: #selector(checkCharBulletPosition), userInfo: nil, repeats: true)
+        charBulletTimer.fire()
+        
         createPlayBackground()
         
         if motionManager.isAccelerometerAvailable == true {
@@ -75,6 +90,7 @@ class PlayScene: SKScene {
                     self.charLocX = currentX + CGFloat((data?.acceleration.x)! * 100)
                     }
                 }
+                //accelerating too fast causes the character to fuse with the screen boundaries --> set a min/max x better
                 self.character.physicsBody?.velocity = CGVector(dx: (data?.acceleration.x)! * 9.0, dy: 0)
             }
         }
@@ -83,14 +99,25 @@ class PlayScene: SKScene {
         character.physicsBody = SKPhysicsBody(circleOfRadius: max(character.size.width / 2, character.size.height / 2))
         character.physicsBody?.affectedByGravity = false
         //0b01
-        character.physicsBody?.collisionBitMask = UInt32(1)
+        //character.physicsBody?.collisionBitMask = UInt32(1)
         //fix this boundary --> character.physicsBody? = SKPhysicsBody(edgeLoopFrom: frame)
         character.physicsBody? = SKPhysicsBody(edgeLoopFrom: CGRect(x: 0, y: 0, width: self.size.width - 50, height: self.size.height))
+    }
+    
+    @objc func checkCharBulletPosition() {
+        //char bullet goes out of bounds
+        for i in 0..<charBullets.count {
+            if(charBullets[i].position.y > self.size.height/4 + 320) {
+                charBullets[i].removeFromParent()
+            }
+        }
         
-        //i is of type SKSpriteNode(references the values in the array)
-        for i in charBullets {
-            if(i.position.x == boss.position.x) {
-                print("touched")
+        //char bullet touches boss
+        for i in 0..<charBullets.count {
+            if(charBullets[i].position.y > boss.position.y - 40 && (charBullets[i].position.x < boss.position.x + 60 && charBullets[i].position.x > boss.position.x - 60)) {
+                charBullets[i].removeFromParent()
+                bossHealth -= 1
+                bossHealthLabel.text = "Boss Health \(bossHealth)"
             }
         }
     }
@@ -113,23 +140,24 @@ class PlayScene: SKScene {
         let followLinePath = SKAction.follow(linePath.cgPath, asOffset: true, orientToPath: false, speed: 50)
         self.boss.run(followLinePath) */
     }
-    
-    @objc func updateBossHealthProgressBar() {
-       // bossHealthBar.setProgress(bossHealthPercentage, animated: true)
-    }
-    
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         let charBullet = SKSpriteNode(texture: charBulletTexture)
-        charBullet.physicsBody = SKPhysicsBody(circleOfRadius: charBullet.size.width/32)
-        charBullet.physicsBody?.usesPreciseCollisionDetection = true
+        charBullet.physicsBody = SKPhysicsBody(circleOfRadius: charBullet.size.width/128)
         charBullet.position = CGPoint(x: character.position.x, y: character.position.y + 100)
         charBullet.zPosition = 1
-        charBullet.physicsBody?.isDynamic = true
+        //charBullet.physicsBody?.isDynamic = true
         charBullet.physicsBody?.affectedByGravity = false
         charBullet.physicsBody?.velocity = CGVector.init(dx: 0, dy: 500)
-        //0b10
-        charBullet.physicsBody?.collisionBitMask = UInt32(2)
-        print(character.position.y)
+        
+        /*if(!(crescendoCategory == 0x1 << 5)) {
+        crescendoCategory <<= 1
+        } else {
+            crescendoCategory = 0x1 << 0
+        }
+        print(crescendoCategory)
+        charBullet.physicsBody?.categoryBitMask = crescendoCategory */
+        
         addChild(charBullet)
         charBullets.append(charBullet)
     }
@@ -153,7 +181,6 @@ class PlayScene: SKScene {
         // Called before each frame is rendered
         let moveCharX = SKAction.moveTo(x: charLocX, duration: 0.08)
         self.character.run(moveCharX)
-        //perform(#selector(updateBossHealthProgressBar), with: nil)
         goThroughSpace()
     }
 }
